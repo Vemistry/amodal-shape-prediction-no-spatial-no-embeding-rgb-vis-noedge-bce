ccf667a344c84e4a4ee60289bc8c800bdd7c2f61
vemistry <phanquangnghia220405@gmail.com>
2026-09-24 10:34:23 +0700

    Updatemodel

--- scripts/model.py
+++ scripts/model.py
@@ -84,6 +84,8 @@ class UpBlock(nn.Module):
         """
         # Phóng tỉ lệ lên 2 lần
         x_up = self.up(x_decoder)
+        x_up = None
+        
         # Nối chiều kênh: [skip, upsampled]
         x_concat = torch.cat([x_skip, x_up], dim=1)
         # Xử lý tích chập trên kết hợp
